781fde5ad831ab63090370174adc95bbb48f1336
Albert Sh <at271989@gmail.com>
2024-10-14 14:17:00 +0300

    Первый коммит лендинга асутп на FLASK в ветке master

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+# Главная страница
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# Маршрут для обработки данных формы (например, заявка от клиента)
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    # Здесь можно добавить логику для обработки данных, например, отправку письма
+    # или сохранение данных в базу данных
+
+    return f"Спасибо, {name}, ваша заявка принята!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
